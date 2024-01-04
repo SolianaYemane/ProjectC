4901d1af0e4501649e56b16b7bb85814a298028b
SolianaYemane <155404423+SolianaYemane@users.noreply.github.com>
2024-01-03 18:39:55 -0800

    Update DemoC.java

--- src/c/DemoC.java
+++ src/c/DemoC.java
@@ -4,6 +4,7 @@ public class DemoC {
 	public static void main(String[] args) {
 		System.out.println("Wendesday");  
 		System.out.println("Sprit 3 Done");  
+		System.out.println("Sprit 4 Done");  
 
 	}
  
